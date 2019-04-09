8ed034071e38c0e2780d6f9d4b8a776e8a4b0fdd
Chirath R <chirath.02@gmail.com>
2019-04-09 12:59:50 +0530

    implenment navigation using react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="nav">
+        <NavLink exact className="nav-link" activeClassName="nav-active" to="/">Home</NavLink>
+        <NavLink className="nav-link" activeClassName="nav-active" to="/courses">Courses</NavLink>
+        <NavLink className="nav-link" activeClassName="nav-active" to="/about">About</NavLink>
+      </nav>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administartion</h1>
+        <p>React, Redux and React Router for ultra-responsive web apps.</p>
+        <button className="btn btn-primary">Learn more</button>
+      </div>
+    );
+  }
+}
+
+class Course extends Component {
+  render() {
+    return (
+      <h1>Courses</h1>
+    );
+  }
+}
+
+class About extends Component {
+  render() {
+    return (
+      <h1>About</h1>
+    )
+  }
+}
+
+class NoMatch extends Component {
+  render() {
+    return (
+      <h1>Ooops! Page not found.</h1>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+            <NavBar/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/courses" component={Course}/>
+            <Route path="/about" component={About}/>
+            <Route component={NoMatch} />
+        </div>
+      </Router>
     );
   }
 }
