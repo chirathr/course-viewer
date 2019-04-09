85772210fcdcd0ea59daf97c36d1774caf1276ef
Chirath R <chirath.02@gmail.com>
2019-04-09 15:37:05 +0530

    update Course page

--- src/App.js
+++ src/App.js
@@ -29,10 +29,74 @@ class Home extends Component {
   }
 }
 
-class Course extends Component {
+class CourseList extends Component {
+  render() {
+    let courses_list = this.props.courses.map((course) => <li>{course}</li>);
+    return <ul>{courses_list}</ul>;
+  }
+}
+
+class AddCourse extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.onSave = this.onSave.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onSave() {
+    this.props.addCourse(this.state.value);
+    this.setState({value: ''});
+  }
+
+  onChange(e) {
+    let value = e.target.value.trim();
+    this.setState({value: value});
+  }
+
   render() {
     return (
-      <h1>Courses</h1>
+      <div className="pt-3">
+        <h4>Add Course</h4>
+        <div className="row">
+          <div className="input-group mt-2 col-4">
+            <input type="text" className="form-control" value={this.state.value} onChange={this.onChange} placeholder="Course name"/>
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button" onClick={this.onSave}>Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Course extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: ['React', 'Redux', 'React-router'],
+    };
+    this.addCourse = this.addCourse.bind(this);
+  }
+
+  addCourse(course) {
+    if (course.trim() === '') {
+      return;
+    }
+    let courses = this.state.courses.slice();
+    courses.push(course);
+    this.setState({courses: courses});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <CourseList courses={this.state.courses} />
+        <AddCourse addCourse={this.addCourse} />
+      </div>
     );
   }
 }
